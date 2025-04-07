14a9b56d1ac55cb9dade20f95d2eeee68d20c759
ZephyrBlitz <gmsarzona@gmail.com>
2025-04-07 10:13:50 +0800

    Vue Installation

---
+++ app.js
@@ -0,0 +1,12 @@
+const app   = Vue.createApp({
+    data() {
+        return {
+            title: 'Noli Me Tangere',
+            author: 'José Rizal',
+            age: 35
+        }
+    }
+
+})
+
+app.mount('#app')
